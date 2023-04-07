726e127d1fcb187a2cd7a8b3ad4758c721e2a88f
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2023-04-07 21:24:49 +0300

    e

--- Museum_UIKit/Utilities/Constants/Password.swift
+++ Museum_UIKit/Utilities/Constants/Password.swift
@@ -7,3 +7,9 @@
 
 import Foundation
 // try
+enum Password {
+    
+    static let wikiEndpoint = "https://en.wikipedia.org/w/api.php"
+    /// delete &exintro= if you need more info from  your query
+    static let wikiBaseURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&pithumbsize=500&format=json&explaintext=&indexpageids&exintro=&titles="
+}
