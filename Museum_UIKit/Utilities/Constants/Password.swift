610edc7d733cd74f6bf40541be5920f9d73c7eaa
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2023-04-11 21:43:49 +0300

    delete pass

--- Museum_UIKit/Utilities/Constants/Password.swift
+++ Museum_UIKit/Utilities/Constants/Password.swift
@@ -8,18 +8,5 @@
 import Foundation
 // try
 enum Password {
-    /// ignore
-    static let wikiEndpoint = "https://en.wikipedia.org/w/api.php"
-    /// delete &exintro= if you need more info from  your query
-    static let wikiBaseURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&pithumbsize=500&format=json&explaintext=&indexpageids&exintro=&titles="
-    static let qwikiEndpoint = "https://en.wikipedia.org/w/api.php"
-    /// delete &exintro= if you need more info from  your query
-    static let qwikiBaseURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&pithumbsize=500&format=json&explaintext=&indexpageids&exintro=&titles="
-    static let wwikiEndpoint = "https://en.wikipedia.org/w/api.php"
-    /// delete &exintro= if you need more info from  your query
-    static let wwikiBaseURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&pithumbsize=500&format=json&explaintext=&indexpageids&exintro=&titles="
-}
-
-enum FakeEnum {
-    case ignore
+   
 }
