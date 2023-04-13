dec1dc91ec058753530dfc5a179613e7a8f06359
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2023-04-13 21:42:38 +0300

    up date

--- Museum_UIKit/Utilities/Constants/Password.swift
+++ Museum_UIKit/Utilities/Constants/Password.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum Password {
-    static let serpapiKey = "ecb4d51fdb1427f90e71de5a17be01538bc7159690eea94bb3c43abdf1d8ff4f"
-    static let imgurKey = "7384944506e4c55"
+    /// Realy?
+
 }
